99951aeaeea2f5869f3f6c3ce41349c7ac02faf3
proelbtn <proelbtn@users.noreply.github.com>
2018-03-27 21:59:39 +0900

    [ADD] prepare the files for creating rootfs

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+# Requirement
+apt update
+apt install -y \
+    apt-transport-https ca-certificates \
+    curl software-properties-common qemu-user-static
+
+# Docker CE
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt update
+apt-get install -y docker-ce
+
+# for Docker
+gpasswd -a vagrant docker
