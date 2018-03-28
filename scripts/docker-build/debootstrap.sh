df21313242dab4f700768323a70a758125df6484
proelbtn <proelbtn@users.noreply.github.com>
2018-03-28 18:53:11 +0900

    [FIX] fix some points to build properly

--- scripts/docker-build/debootstrap.sh
+++ scripts/docker-build/debootstrap.sh
@@ -3,14 +3,16 @@
 set -e
 cd $(dirname $0)/../..
 
-if [ ! -d /tmp/rootfs ]
+mkdir -p /pi-oven
+
+if [ ! -d /pi-oven/rootfs ]
 then
     qemu-debootstrap --components=main,contrib,non-free \
             --arch armhf \
             --keyring lib/raspberrypi.gpg \
-            stretch /tmp/rootfs http://mirrordirector.raspbian.org/raspbian/
+            stretch /pi-oven/rootfs http://mirrordirector.raspbian.org/raspbian/
 fi
 
 docker image build \
         -t pi-oven-debootstrap \
-        -f docker/debootstrap/Dockerfile /tmp
+        -f docker/debootstrap/Dockerfile $@ /pi-oven
--- scripts/docker-build/stage0.sh
+++ scripts/docker-build/stage0.sh
@@ -5,4 +5,4 @@ cd $(dirname $0)/../..
 
 docker image build \
         -t pi-oven-stage0 \
-        -f docker/stage0/Dockerfile docker/stage0
+        -f docker/stage0/Dockerfile $@ docker/stage0
